3393f73ec63e04137fafbd6b4f73a54409a30bef
yuli <yuliana@dormify.com>
2023-02-28 22:01:22 -0300

    get products in home

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,23 @@
 import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 const Home = () => {
-    return <h1>Home</h1>;
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/product")
+            .then(res => {
+                const newProducts = res.data;
+                console.log("newProducts", newProducts)
+                setProducts(newProducts)
+        })
+    },[])
+    console.log(products)
+
+    return <div>
+        <h1>Home</h1>
+        {products?.map(prod => (<div>{prod?.title}</div>))}
+        </div>;
   };
   
   export default Home;
--- server/models/product.model.js
+++ server/models/product.model.js
@@ -5,4 +5,4 @@ const ProductSchema = new mongoose.Schema({
     price: { type: Number },
     description: {type: String}
 }, { timestamps: true });
-module.exports.Product = mongoose.model('Product', ProductSchema);
+module.exports.Product = mongoose.model('Product', ProductSchema, 'product');
